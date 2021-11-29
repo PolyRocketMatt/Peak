177e395af5eeb13ed1d2efa4bddd6d3e94ebbee4
Matthias <rocketpencil.studios@gmail.com>
2021-11-29 02:09:59 +0100

    Can I start writing code now?

--- src/main/kotlin/com/github/polyrocketmatt/peak/Peak.kt
+++ src/main/kotlin/com/github/polyrocketmatt/peak/Peak.kt
@@ -1,5 +1,11 @@
 package com.github.polyrocketmatt.peak
 
+import com.github.polyrocketmatt.game.Vec2f
+
 fun main(args: Array<String>) {
     println("YAY")
+
+    val v = Vec2f(0.5f, 1.0f)
+
+    println(v)
 }
