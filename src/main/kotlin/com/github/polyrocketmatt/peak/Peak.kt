b8f64630d3b0d964151a456811b3d036cd9da9cf
Matthias <rocketpencil.studios@gmail.com>
2021-11-29 03:13:08 +0100

    Why doesn't git update...

--- src/main/kotlin/com/github/polyrocketmatt/peak/Peak.kt
+++ src/main/kotlin/com/github/polyrocketmatt/peak/Peak.kt
@@ -1,11 +1,5 @@
 package com.github.polyrocketmatt.peak
 
-import com.github.polyrocketmatt.game.Vec2f
-
 fun main(args: Array<String>) {
-    println("YAY")
-
-    val v = Vec2f(0.5f, 1.0f)
-
-    println(v)
+    println("PLEASE UPDATE GIT")
 }
